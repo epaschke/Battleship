f7ce3b0f1ece53ac291ee2b90c662705c965e662
Emma Paschke <epaschke@outlook.com>
2018-04-03 15:18:55 -0700

    update layout and move game functions to object to reduce collisions

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,12 +1,12 @@
-var direct = "http://localhost:8080/"
-var playerId;
-var state = {
+const direct = "http://localhost:8080/"
+const state = {
   currentShip: null,
   count: 0,
   start: [],
   end: []
 };
-var interval;
+const gameFunctions = {};
+let interval;
 
 $(document).ready(function(){
 
@@ -17,15 +17,24 @@ $(document).ready(function(){
        if (resp.success) { //if there is room in the game for another player
          //all hidden divs are shown
          $("#player").text(resp.player);
-         playerId = () => parseInt($("#player").text());
+         gameFunctions.playerId = () => parseInt($("#player").text());
          $("#info").css("display", "flex");
          $("#joinGame").hide();
          $(".buttons").show();
          $("#board-container").show();
-         $("#prompt").show();
-         $("#prompt").text("To place ships of 5 different lengths, click on a button below.");
-         //interval is set to ping backend for changes every 3 seconds to update game board
-         interval = setInterval(getChanges, 3000);
+         $("#prompt").show().text("To place ships of 5 different lengths, click on a button below.");
+         for (let i = 0; i < 10; i++){
+           $("#playerBoard").append(`<div id="playerBoard${i}"><div class="label">${i}</div></div>`);
+           $("#attackBoard").append(`<div id="attackBoard${i}"><div class="label">${i}</div></div>`);
+         }
+        for (let i = 0; i < 10; i++){
+          for (let j = 0; j < 10; j++){
+            $(`#playerBoard${i}`).append(`<div id="p${i}${j}" class="PlayerBoardCell water"></div>`);
+            $(`#attackBoard${i}`).append(`<div id="p${i}${j}" class="AttackBoardCell water"></div>`);
+          }
+        }
+         // interval is set to ping backend for changes every 3 seconds to update game board
+         interval = setInterval(gameFunctions.getChanges, 3000);
        } else {
          $("#prompt").text(resp.errorMessage);
        }
@@ -47,100 +56,57 @@ $(document).ready(function(){
   });
 
 //when user clicks on a ship button
-  $(".ship-button").click(function(event){
-    $(this).css("background-color", "red"); //set button color to red
+  $(".ship-button").click(function (e) {
+    $(this).addClass("selected");
     state.currentShip = this.id; //set current ship to button id
     $("#prompt").text(`Select a start and end for a ship of length ${state.currentShip.split("ship")[1]}!`)
   });
 
   //when user clicks on a cell of the Player Board
-  $(".PlayerBoardCell").click( function(event){
+  $("#playerBoard").click(".PlayerBoardCell", function (e) {
     //if no ship button has been selected
     if (!state.currentShip){
       $("#prompt").text("Select a ship!");
      //if cell is the first cell clicked for this ship
-    } else if (!state.count){
-      //if cell already has a ship
-      if ($(this).css("background-color") !== "rgb(255, 165, 0)") {
+    } else if ($(e.target).hasClass("water")) {
+       if (!state.count){
         state.shipLength = state.currentShip.split("ship")[1];
-        state.startId = this.id;
-        state.start = this.id.split("");
-        state.start.shift();
-        $(this).css("background-color", "orange");
+        state.startId = e.target.id;
+        state.start = e.target.id.split("");
+        $(e.target).addClass("ship").removeClass("water");
         state.count++;
-        $("#prompt").text(`Start selected for ship ${state.shipLength}. Now select endpoint.`)
+        if (state.shipLength == 1){
+          gameFunctions.attemptSetShip(e.target);
+        } else {
+          $("#prompt").text(`Start selected for ship ${state.shipLength}. Now select endpoint.`)
+        }
+    //if cell is second cell clicked for this ship
       } else {
+        gameFunctions.attemptSetShip(e.target);
+      }
+    } else {
         $("#prompt").text("You already placed a ship there.")
       }
-    //if cell is second cell clicked for this ship
-    } else {
-      state.end = this.id.split("");
-      state.end.shift();
-      var _this = this;
-      $(this).css("background-color", "orange");
-      //request backend to verify ship placement
-      $.ajax({
-        "url": direct + `setShip/${playerId()}`,
-        method: 'POST',
-        headers: {
-          "content-type": "application/json",
-          "cache-control": "no-cache"
-        },
-        data: JSON.stringify({ "shipLength": parseInt(state.shipLength), "start1": parseInt(state.start[0]), "start2": parseInt(state.start[1]), "end1": parseInt(state.end[0]), "end2": parseInt(state.end[1]) })
-      }).done(function(resp){
-        // if ship placement is valid, fill in orange squares between start and end
-        if (resp.success) {
-            // if ship is horizontal
-            if (state.start[0] === state.end[0]){
-                let lesser = state.start[1] > state.end[1] ? state.end[1] : state.start[1];
-                let greater = state.start[1] < state.end[1] ? state.end[1] : state.start[1];
-                for (let i = lesser; i < greater; i++){
-                    $(`#p${state.start[0]}${i}`).css("background-color", "orange");
-                }
-            //if ship is vertical
-            } else {
-                let lesser = state.start[0] > state.end[0] ? state.end[0] : state.start[0];
-                let greater = state.start[0] < state.end[0] ? state.end[0] : state.start[0];
-                for (let i = lesser; i < greater; i++){
-                    $(`#p${i}${state.start[1]}`).css("background-color", "orange");
-                }
-            }
-          $("#prompt").text("Ship was placed!")
-        // if ship placement is invalid, change squares back to white
-        } else {
-          $(`#${state.startId}`).css("background-color", "white");
-          $(_this).css("background-color", "white");
-          $(`#${state.currentShip}`).css("background-color", "white");
-          $("#prompt").text(resp.errorMessage);
-        }
-        //reset ship state
-        state.currentShip = null;
-        state.count = 0;
-      })
-      .catch(function(error){
-        console.log(error);
-      })
-    }
   });
 
   //when user clicks cell on attack board
-  $(".AttackBoardCell").click(function(event){
-      var x = this.id.split("");
-      var _this = this;
+  $("#attackBoard").click(".AttackBoardCell", function (e){
+      let x = e.target.id.split("");
+      let _this = e.target;
     //verify move with backend
     $.ajax({
-        "url": direct + `move/${playerId()}`,
+        "url": direct + `move/${gameFunctions.playerId()}`,
         method: 'POST',
         headers: {
           "content-type": "application/json",
           "cache-control": "no-cache"
         },
-        data: JSON.stringify({ "row": x[0], "column": x[1] })
+        data: JSON.stringify({ "row": parseInt(x[1]), "column": parseInt(x[2]) })
     }).done(function(resp){
       //move is valid, change color based on whether it is a hit or miss
       if (resp.success){
-        var color = resp.correctHit ? 'green' : 'red';
-        $(_this).css("background-color", color);
+        let classToAdd = resp.correctHit ? 'hit' : 'miss';
+        $(_this).addClass(classToAdd).removeClass('water');
         var text = resp.correctHit ? "Good hit!" : "Miss!";
         $("#prompt").text(text);
       //move is invalid
@@ -153,36 +119,87 @@ $(document).ready(function(){
     })
   });
 
-  //polls backend for changes to player board state
-  function getChanges() {
-    $.ajax({"url": direct + `getChanges/${playerId()}`})
-    .done(function(resp){
-      if (resp.success){
-        //if game is started
-        if (resp.whoseTurn !== 0) {
-        //if cell was hit, change color
-          for (let rowId = 0; rowId < 10; rowId++){
-            resp.board[`r${rowId}`].map((cell, cellId) => {
-                  if (cell === "x") {
-                    $(`#p${rowId}${cellId}`).css("background-color", "green");
-                  } else if (cell === "o") {
-                    $(`#p${rowId}${cellId}`).css("background-color", "lightgray");
-                  }
-            });
-          }
-          //update prompt based on player
-          $("#prompt").text(`It's ${resp.whoseTurn === playerId() ? "your turn. Click anywhere on the attack board." : "their turn!"}`)
-          //if game is over
-          if (resp.gameOver){
-             $("#prompt").text(`Game is over. ${resp.winner === playerId() ? "You won!" : "You lost!"}`);
-             clearInterval(interval);
-           }
-        }
-      }
-    })
-    .catch(function(error){
-      console.log(error);
-    });
-  }
-
 });
+
+// GAME FUNCTIONS
+
+gameFunctions.attemptSetShip = (_this) => {
+  state.end = _this.id.split("");
+  $(_this).addClass("ship").removeClass("water");
+  $.ajax({
+    "url": direct + `setShip/${gameFunctions.playerId()}`,
+    method: 'POST',
+    headers: {
+      "content-type": "application/json",
+      "cache-control": "no-cache"
+    },
+    data: JSON.stringify({ "shipLength": parseInt(state.shipLength), "start1": parseInt(state.start[1]), "start2": parseInt(state.start[2]), "end1": parseInt(state.end[1]), "end2": parseInt(state.end[2]) })
+  }).done(function(resp){
+    if (resp.success) { // if ship placement is valid, fill in ship squares to colors
+      gameFunctions.changeShipSquares();
+    } else { // if ship placement is invalid, change squares back to water
+      $(`#${state.startId}`).addClass("water").removeClass("ship");
+      $(_this).addClass("water").removeClass("ship");
+      $(`#${state.currentShip}`).removeClass("selected");
+      $("#prompt").text(resp.errorMessage);
+    }
+    //reset ship state
+    state.currentShip = null;
+    state.count = 0;
+  })
+  .catch(function(error){
+    console.log(error);
+  })
+}
+
+gameFunctions.changeShipSquares = () => {
+  console.log(state);
+  // if ship is horizontal
+  if (state.start[1] === state.end[1]){
+      let lesser = state.start[2] > state.end[2] ? state.end[2] : state.start[2];
+      let greater = state.start[2] < state.end[2] ? state.end[2] : state.start[2];
+      for (let i = lesser; i < greater; i++){
+          $(`#p${state.start[1]}${i}`).addClass("ship").removeClass("water");
+      }
+  //if ship is vertical
+  } else {
+      let lesser = state.start[1] > state.end[1] ? state.end[1] : state.start[1];
+      let greater = state.start[1] < state.end[1] ? state.end[1] : state.start[1];
+      for (let i = lesser; i < greater; i++){
+          $(`#p${i}${state.start[2]}`).addClass("ship").removeClass("water");
+      }
+  }
+  $("#prompt").text("Ship was placed!")
+}
+
+  //polls backend for changes to player board state
+gameFunctions.getChanges = () => {
+  $.ajax({"url": direct + `getChanges/${gameFunctions.playerId()}`})
+  .done(function(resp){
+    if (resp.success){
+      //if game is started
+      if (resp.whoseTurn !== 0) {
+      //if cell was hit, change color
+        for (let rowId = 0; rowId < 10; rowId++){
+          resp.board[`r${rowId}`].map((cell, cellId) => {
+                if (cell === "x") {
+                  $(`#p${rowId}${cellId}`).addClass("oppHit").removeClass("water");
+                } else if (cell === "o") {
+                  $(`#p${rowId}${cellId}`).addClass("oppMiss").removeClass("water");
+                }
+          });
+        }
+        //update prompt based on player
+        $("#prompt").text(`It's ${resp.whoseTurn === gameFunctions.playerId() ? "your turn. Click anywhere on the attack board." : "their turn!"}`)
+        //if game is over
+        if (resp.gameOver){
+           $("#prompt").text(`Game is over. ${resp.winner === gameFunctions.playerId() ? "You won!" : "You lost!"}`);
+           clearInterval(interval);
+         }
+      }
+    }
+  })
+  .catch(function(error){
+    console.log(error);
+  });
+}
